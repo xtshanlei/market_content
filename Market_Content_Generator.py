9527a0d54831f70098b9ee57f7e08929fbc2b85b
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2023-03-06 10:53:38 +0000

    Update Market_Content_Generator.py

--- Market_Content_Generator.py
+++ Market_Content_Generator.py
@@ -9,7 +9,7 @@ def text_generation(content_types, bullet_points):
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",temperature=0.7,max_tokens=900,
       messages=[
-            {"role": "system", "content": "You are an expert in the marketing industry. Write a {} based on the bullet points from the message. Make sure it's funny and easy to read. Your slogan is 'Doing it Differently!'".format(content_types)},
+            {"role": "system", "content": "You are an expert in the marketing industry. Write a {} based on the bullet points from the message. Make sure it's funny and easy to read. Your slogan is 'Doing it Differently!'. If it's a blog, the word counts should be around 500.".format(content_types)},
             {"role": "user", "content": "{}".format(bullet_points)},
         ]
     )
@@ -18,7 +18,7 @@ def text_generation(content_types, bullet_points):
 
 content_types = ', and '.join(st.multiselect('What types of content do you want to generate?',['blog','tweet','facebook post'],['blog']))
 bullet_points = st.text_area('Bullet points for your content:')
-st.write(bullet_points)
+
 if st.button('Generate!'):
     response = text_generation(content_types,bullet_points)
     st.write(response)
