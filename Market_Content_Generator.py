000d53e041bd92a29e1a6ee3f6182ca8d273aca9
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2023-03-06 10:21:30 +0000

    Update Market_Content_Generator.py

--- Market_Content_Generator.py
+++ Market_Content_Generator.py
@@ -4,3 +4,14 @@ import openai
 
 st.title('Automatic Marketing Content Generator')
 openai.api_key = st.secrets['api_key']
+
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",temperature=7,max_tokens=256,
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+st.write(response[‘choices’][0][‘message’][‘content’])
