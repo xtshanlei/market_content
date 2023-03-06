757df08c2d80d4ce07e396511c40ffa997ce9d36
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2023-03-06 10:31:56 +0000

    Update Market_Content_Generator.py

--- Market_Content_Generator.py
+++ Market_Content_Generator.py
@@ -10,11 +10,11 @@ def text_generation(bullet_points):
       model="gpt-3.5-turbo",temperature=0.7,max_tokens=900,
       messages=[
             {"role": "system", "content": "You are an expert in the marketing industry. Write a blog based on the bullet points from the message. Make the blog funny and easy to read. Then write a tweet based on the blog. Your slogan is 'Doing it Differently!'"},
-            {"role": "user", "content": {}.format(bullet_points)},
+            {"role": "user", "content": "{}".format(bullet_points)},
         ]
     )
     return response['choices'][0]['message']['content']
 
 bullet_points = st.text_area('Bullet points for your content:')
-if bullet_points:
+if st.button('Generate your marketing button'):
     st.write(text_generation(bullet_points))
