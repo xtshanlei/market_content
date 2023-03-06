da1cc9b2593149ebc1cdfd75c06862738bae1198
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2023-03-06 10:22:27 +0000

    Update Market_Content_Generator.py

--- Market_Content_Generator.py
+++ Market_Content_Generator.py
@@ -6,7 +6,7 @@ st.title('Automatic Marketing Content Generator')
 openai.api_key = st.secrets['api_key']
 
 response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",temperature=7,max_tokens=256,
+  model="gpt-3.5-turbo",temperature=0.7,max_tokens=256,
   messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Who won the world series in 2020?"},
