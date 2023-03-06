d8b73af2741c90a603b9acb4dd9bb35f6193f251
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2023-03-06 10:18:05 +0000

    Update Market_Content_Generator.py

--- Market_Content_Generator.py
+++ Market_Content_Generator.py
@@ -1,2 +1,6 @@
 import streamlit as st
 import openai
+
+---------------------------------------------------------------
+st.title('Automatic Marketing Content Generator')
+openai.api_key = st.secrets['api_key']
