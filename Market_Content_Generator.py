b10526ffb29d88e375c37d6c88b7ec18263c9b28
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2023-03-06 10:55:56 +0000

    Update Market_Content_Generator.py

--- Market_Content_Generator.py
+++ Market_Content_Generator.py
@@ -5,11 +5,11 @@ import openai
 st.title('Automatic Marketing Content Generator')
 openai.api_key = st.secrets['api_key']
 
-def text_generation(content_types, bullet_points):
+def text_generation(content_types, bullet_points,slogan):
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",temperature=0.7,max_tokens=900,
       messages=[
-            {"role": "system", "content": "You are an expert in the marketing industry. Write a {} based on the bullet points from the message. Make sure it's funny and easy to read. Your slogan is 'Doing it Differently!'. If it's a blog, the word counts should be around 500.".format(content_types)},
+            {"role": "system", "content": "You are an expert in the marketing industry. Write a {} based on the bullet points from the message. Make sure it's funny and easy to read. Your slogan is {}. If it's a blog, the word counts should be around 500.".format(content_types,slogan)},
             {"role": "user", "content": "{}".format(bullet_points)},
         ]
     )
@@ -17,8 +17,9 @@ def text_generation(content_types, bullet_points):
 
 
 content_types = ', and '.join(st.multiselect('What types of content do you want to generate?',['blog','tweet','facebook post'],['blog']))
+slogan = st.text('What is your marketing slogan?')
 bullet_points = st.text_area('Bullet points for your content:')
 
 if st.button('Generate!'):
-    response = text_generation(content_types,bullet_points)
+    response = text_generation(content_types,bullet_points,slogan)
     st.write(response)
